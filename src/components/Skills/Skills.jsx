50e3d680c47e5c1b1172f4b3a3c087025349d549
rzyfront <rzyfront@gmail.com>
2025-03-27 18:45:06 -0500

    Enhance Skills component by adding new technologies and improving code structure

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -6,13 +6,16 @@ import Img1 from '../../assets/ImageSide1.png'
 import Img2 from '../../assets/ImageSide2.png'
 import Img3 from '../../assets/ImageSide3.png'
 import Img4 from '../../assets/ImageSide4.png'
-import {SiJavascript,SiNodedotjs,SiReact,SiExpress,SiPostgresql,SiMongodb,SiHtml5,SiCss3,SiRedux,SiGit,SiGithub,SiSass,SiJest,SiPhotopea,SiAdobephotoshop,SiFigma,SiBootstrap,SiTailwindcss,SiMysql,SiTypescript} from 'react-icons/si'
+import {
+  SiJavascript, SiNodedotjs, SiReact, SiExpress, SiPostgresql, SiMongodb, SiHtml5, 
+  SiCss3, SiRedux, SiGit, SiGithub, SiSass, SiJest, SiPhotopea, SiAdobephotoshop, 
+  SiFigma, SiBootstrap, SiTailwindcss, SiMysql, SiTypescript, SiNextdotjs, 
+  SiMicrosoftsqlserver, SiOracle, SiPhp, SiLaravel, SiVuedotjs, SiPython, SiDjango
+} from 'react-icons/si'
 import { useSelector } from 'react-redux';
-
 
 function Skills() {
       const {Languages} = useSelector(state=>state)
-
     const Images = [
     Img1,
     Img2,
@@ -40,41 +43,34 @@ function Skills() {
         <div className='Skill-Tag'><SiHtml5/><p>HTML</p></div>
         <div className='Skill-Tag'><SiCss3/><p>CSS</p></div>
         <div className='Skill-Tag'><SiJavascript/><p>JavaScript</p></div>
-         <div className='Skill-Tag'><SiTypescript/><p>TypeScript</p>
-        </div>
-        <div className='Skill-Tag'><SiNodedotjs/><p>Node.js</p></div>
+        <div className='Skill-Tag'><SiTypescript/><p>TypeScript</p></div>
         <div className='Skill-Tag'><SiReact/><p>React.js</p></div>
+        <div className='Skill-Tag'><SiNextdotjs/><p>Next.js</p></div>
+        <div className='Skill-Tag'><SiVuedotjs/><p>Vue.js</p></div>
+        <div className='Skill-Tag'><SiNodedotjs/><p>Node.js</p></div>
+        <div className='Skill-Tag'><SiTailwindcss/><p>Tailwindcss</p></div>
+        <div className='Skill-Tag'><SiBootstrap/><p>Bootstrap</p></div>
+        <div className='Skill-Tag'><SiPhp/><p>PHP</p></div>
+        <div className='Skill-Tag'><SiLaravel/><p>Laravel</p></div>
+        <div className='Skill-Tag'><SiPython/><p>Python</p></div>
+        <div className='Skill-Tag'><SiDjango/><p>Django</p></div>
         <div className='Skill-Tag'><SiExpress/><p>Express.js</p></div>
-        <div className='Skill-Tag'><SiPostgresql/><p>PostgresSql</p></div>
+        <div className='Skill-Tag'><SiPostgresql/><p>PostgreSQL</p></div>
+        <div className='Skill-Tag'><SiMysql/><p>MySQL</p></div>
+        <div className='Skill-Tag'><SiMicrosoftsqlserver/><p>MS SQL Server</p></div>
+        <div className='Skill-Tag'><SiOracle/><p>Oracle</p></div>
         <div className='Skill-Tag'><SiMongodb/><p>MongoDB</p></div>
         <div className='Skill-Tag'><SiRedux/><p>Redux</p></div>
         <div className='Skill-Tag'><SiGit/><p>Git</p></div>
-        <div className='Skill-Tag'><SiGithub/><p>GitHub</p>
+        <div className='Skill-Tag'><SiGithub/><p>GitHub</p></div>
+        <div className='Skill-Tag'><SiSass/><p>Sass</p></div>
+        <div className='Skill-Tag'><SiJest/><p>Jest.js</p></div>
+        <div className='Skill-Tag'><SiPhotopea/><p>Photopea</p></div>
+        <div className='Skill-Tag'><SiAdobephotoshop/><p>Photoshop</p></div>
+        <div className='Skill-Tag'><SiFigma/><p>Figma</p></div>
         </div>
-        <div className='Skill-Tag'><SiSass/><p>Sass</p>
-        </div>
-        <div className='Skill-Tag'><SiJest/><p>Jest.js</p>
-        </div>
-        <div className='Skill-Tag'><SiPhotopea/><p>Photopea</p>
-        </div>
-        <div className='Skill-Tag'><SiAdobephotoshop/><p>Photoshop</p>
-        </div>
-        <div className='Skill-Tag'><SiFigma/><p>Figma</p>
-        </div>
-        <div className='Skill-Tag'><SiBootstrap/><p>Booststrap</p>
-        </div>
-        <div className='Skill-Tag'><SiTailwindcss/><p>Tailwindcss</p>
-        </div>
-        <div className='Skill-Tag'><SiMysql/><p>MySQL</p>
-        </div>
-
-        </div>
-        
-        
-        
       </div>
     </div>
   )
 }
-
 export default Skills
